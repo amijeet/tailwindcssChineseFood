501125c7c0bcec7a3b697c1301cb276e6dab61d6
amijeet <amijeetthakur@gmail.com>
2021-04-24 23:36:26 +0530

    chinese food website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: "#FF6363",
+				secondary: {
+					100: "#E2E2D5",
+					200: "#888883",
+				},
+			},
+			fontFamily: {
+				body: ["Nunito"],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
